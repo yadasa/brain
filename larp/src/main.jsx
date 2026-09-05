bce0158d02f70e860ee09b36a9f5b70022522561
Kei <75089908+yadasa@users.noreply.github.com>
2026-09-05 16:10:38 -0500

    larp: load trading mode styles

--- larp/src/main.jsx
+++ larp/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './larp-effects.css'
+import './trading.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
